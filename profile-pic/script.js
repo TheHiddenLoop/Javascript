42b37c725699374df8b4e8b08056d24dc3dc23d8
Angesh0007 <chauhanangesh60@gmail.com>
2025-05-12 11:05:07 +0530

    Add .gitignore

--- profile-pic/script.js
+++
@@ -1,8 +0,0 @@
-const choose=document.getElementById("choose");
-const profilepic=document.getElementById("profile-pic");
-
-
-choose.onchange=function(){
-    profilepic.src=URL.createObjectURL(choose.files[0]);
-}
-
